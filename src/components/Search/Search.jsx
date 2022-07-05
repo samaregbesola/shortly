ac3d863963008e17df3abf20472f1c56581902ff
Samuel Aregbesola <68120291+samaregbesola@users.noreply.github.com>
2022-07-05 08:49:05 +0100

    created custom fetch hook

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
 import "./search.css";
 const Search = () => {
+  const [keyword, setKeyword] = useState("");
+  const [term, setTerm] = useState("");
+  const url = `https://api.shrtco.de/v2/shorten?url=${term}`;
+
+  const { isPending, error, data } = useFetch(url);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTerm(keyword);
+  };
+
+  console.log(data);
+
   return (
-    <form>
-      <div className="search__wrapper">
-        <div className="container search">
-          <input
-            type="text"
-            className="search__input"
-            placeholder="Shorten a link here..."
-          />
-          <button type="submit" className="btn btn--square">
-            Shorten It!
-          </button>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="search__wrapper">
+          <div className="container search">
+            <input
+              type="text"
+              className="search__input"
+              placeholder="Shorten a link here..."
+              onChange={(e) => {
+                setKeyword(e.target.value);
+              }}
+            />
+            <button type="submit" className="btn btn--square">
+              Shorten It!
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
