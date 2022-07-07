5af8860defb28dbe1d43f6b7b80b123a841a6adc
Samuel Aregbesola <68120291+samaregbesola@users.noreply.github.com>
2022-07-07 15:59:45 +0100

    update copy functionality

--- src/components/Search/Results.jsx
+++ src/components/Search/Results.jsx
@@ -1,13 +1,36 @@
+import { useState, useRef } from "react";
 import "./result.css";
 const Results = ({ result }) => {
   const { code, original_link, full_short_link } = result;
+  const copyRef = useRef();
+  const [copy, setCopy] = useState(false);
+
+  const copyFunctionality = () => {
+    navigator.clipboard.writeText(full_short_link).then(
+      function () {
+        setCopy(true);
+        setTimeout(() => {
+          setCopy(false);
+        }, 5000);
+      },
+      function () {
+        alert("copy failed");
+      }
+    );
+  };
   return (
     <article key={code} className="result">
       <p className="result__link">{original_link}</p>
       <div className="result__line"></div>
       <div className="result__content">
         <p className="result__short">{full_short_link}</p>
-        <button className="btn btn--square">Copy</button>
+        <button
+          ref={copyRef}
+          onClick={copyFunctionality}
+          className={`btn btn--square ${copy && `btn--copy`}`}
+        >
+          {copy ? "Copied" : "Copy"}
+        </button>
       </div>
     </article>
   );
